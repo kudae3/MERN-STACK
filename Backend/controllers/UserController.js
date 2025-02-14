aaf35c73bd553ec00b3711c2784fe67917ed5269
Kudae Sithu <kudaesithu2@gmail.com>
2025-02-14 20:47:17 +0630

    Register Api

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -1,10 +1,34 @@
 import User from '../models/User.js';
+import bcrypt from 'bcrypt';
 const UserController = {
+    
+    register: async (req, res) => {
+        try{
+            let {name, email, password} = req.body;
+            let userExists = await User.findOne({email});
+            
+            if(userExists){
+                return res.status(400).json({message: 'User already exists'});
+            }
+            else{
+                let salt = await bcrypt.genSalt();
+                let hash = await bcrypt.hash(password, salt);
+                let user = await User.create({
+                    name, 
+                    email, 
+                    password: hash
+                });
+                return res.status(200).json({message: 'User created successfully', user});
+            }
+        }   
+        catch(error){
+            return res.status(500).json({message: 'Internal Server Error'});
+
+        }     
+    },
+    
     login : (req, res) => {
         return res.json({message: 'Login Api'});
-    },
-    register: (req, res) => {
-        return res.json({message: 'Register Api'});
     }
 }
 
