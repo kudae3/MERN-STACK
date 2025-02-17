1728ea520c12ade44befbd57fc1323454b66a342
Kudae Sithu <kudaesithu2@gmail.com>
2025-02-17 11:38:37 +0630

    Store token at local storage

--- Frontend/src/contexts/AuthContext.jsx
+++ Frontend/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const AuthContext = createContext();
 
@@ -7,10 +7,12 @@ const AuthContext = createContext();
 const reducer = (state, action) => {
     switch (action.type) {
         case "LOGIN":
+            localStorage.setItem("user", JSON.stringify(action.payload));
             return {
                 user: action.payload
             }
         case "LOGOUT":
+            localStorage.removeItem("user");
             return {
                 user: null
             }
@@ -24,6 +26,20 @@ const AuthProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, {
         user: null
     });
+
+    useEffect(() => {
+        try {
+            let user = localStorage.getItem("user");
+            if (user) {
+                dispatch({type: "LOGIN", payload: JSON.parse(user)});
+            }else{
+                dispatch({type: "LOGOUT"});
+            }
+        } catch (e) {
+            console.log(e);
+            dispatch({type: "LOGOUT"});   
+        }
+    }, []);
 
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
